fbcc184813c4990bf7b9c7f1f8dae64d492f1365
alexarirok <akorir233@gmail.com>
2018-08-17 22:30:03 +0300

    The website can respond and submit answers when altenatives are given

--- js/script.js
+++ js/script.js
@@ -3,39 +3,35 @@ function check(){
 var question1= document.quiz.question1.value;
 var question2= document.quiz.question2.value;
 var question3= document.quiz.question3.value;
-var question4= document.quiz.question4.value;
 var correct=0;
-};
 
-if (question1 == "HTML") {
-correct=++;
-};
+if (question1 === "jQuery") {
+correct++;
+}
 
-if (question2 == "JS") {
-correct=++;
-};
+if (question2 === "Brendan Eich") {
+correct++;
+}
 
-if (question3 == "No") {
-correct=++;
-};
+if (question3 === "javaScript") {
+correct++;
+}
 
-var messages = ["Great job!", "That's just okay", "You really need to do better"];
+var messages = ["Exellent!", "Good job", "Good trial!! try again"];
 
 var range;
 
 if (correct < 1)
 {
 range = 2;
-};
-
+}
 if (correct > 0 && correct < 3){
 range = 1;
-};
-
+}
 if (correct > 2){
 range = 0;
-};
+}
 
 document.getElementById("message").innerHTML= messages[range];
-document.getElementById("number_correct").innerHTML = "You got " +correct+ "correct";
-};
+document.getElementById("number_correct").innerHTML = "You got " + correct + " correct ";
+}
